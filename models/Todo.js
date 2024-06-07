74e3ec156093ed334563fce1a93922889a6232d6
Komal Jadhav <komalmjadhav25@gmail.com>
2024-06-07 14:23:27 +0530

    initial setup

---
+++ models/Todo.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose")
+
+const todoShema = new mongoose.Schema({
+    task: {
+        type: String,
+        required: true
+    },
+    desc: {
+        type: String,
+        required: true
+    },
+    complete: {
+        type: Boolean,
+        default: false
+    }
+})
+
+module.exports = mongoose.model("todo", todoShema)
